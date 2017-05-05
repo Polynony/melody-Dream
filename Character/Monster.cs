4489cd29556f32c48529339d7c949894a2e20034
mystery player <choopit009@gmail.com>
2017-05-05 13:22:30 +0800

    Update Monster.cs

--- Character/Monster.cs
+++ Character/Monster.cs
@@ -9,6 +9,11 @@ public class Monster : MonoBehaviour {
 	public float Speed;
 	public bool IsWeak = false;//判断是否触发了弱点
 	protected int[] MonsterWeak;//存储怪物弱点，目前约有16种，受到攻击时遍历
+	
+	public Monster(int monsterhealth, string monstertype){
+		MonsterHealth = monsterhealth;
+		MonsterType = monstertype;
+	}
 
 	public void patrol(){
 		//怪物日常状态，在固定区域随机移动
